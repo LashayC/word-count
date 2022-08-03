4a16ed596f1d7fc9956a5c3f9a102aa0fe2ab82b
LashayC <cokleylashay@gmail.com>
2022-08-03 18:27:53 -0400

    Added Javadoc (in progress)

--- src/main/java/com/tlglearning/wordcount/Main.java
+++ src/main/java/com/tlglearning/wordcount/Main.java
@@ -9,10 +9,32 @@ import java.util.Comparator;
 import java.util.Map;
 import java.util.Map.Entry;
 
-public class Main {
+/**
+ * Demonstrates the practice of word counting, used (for example) in authorship studies. This
+ * demonstration uses (in the included methods) implementations of the {@link java.util.Map},
+ * {@link java.util.Map.Entry}, and {@link java.util.stream.Stream} interfaces.
+ */
+public final class Main {
 
   public static final String TEST_FIlE_NAME = "hound-of-the-baskervilles.txt"; //TIP this constant was by clicking on file name > Refactor> Extract > Constant > Then change the name and create.
+  //NOTE everything below is a complete javadoc comment for the main method below.
 
+  private Main(){
+    //NOTE javadocs only generates external documentation for things that are protected or public, not private?
+  }
+
+  /**
+   * Creates an instance of {@link WordCounter}, using it to process the contents of "The Hound of
+   * the Baskervilles", by Arthur Conan Doyle. After processing, the word counts (excluding short words) are
+   * listed in descending order.
+   * <p>You can use HTML tags here! the documentation in the ratingAnalyzer is html!!!
+   * This method assumes that a text file named {@code hound-of-the-baskervilles.text} is loacted
+   * on the classpath; otherwise (or if that file cannnot be read for some reason), an instance of
+   * {@link IOException} is thown</p>
+   *
+   * @param args Command-line arguments (currently ignored).
+   * @throws IOException Thrown if input file cannot be located or read.
+   */
   public static void main(String[] args) throws IOException {
 
     try (
@@ -27,8 +49,6 @@ public class Main {
         //TODO Pass line to a method of WordCounter.
         counter.add(line);
       }
-      //TODO Do something with the WordCounter.
-//      System.out.println(counter);
 
       counter
           .getCounts()
